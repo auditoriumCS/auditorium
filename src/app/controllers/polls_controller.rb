07faef305baf43d5b878ba4aeda1551935e22aa4
Heinrich Kuhlmann <HeinrichKuhlmann@gmail.com>
2013-05-16 14:44:58 +0200

    polls_controller dummy data

--- src/app/controllers/polls_controller.rb
+++ src/app/controllers/polls_controller.rb
@@ -5,19 +5,19 @@ class PollsController < ApplicationController
   def index
     @polls = Poll.all
 
-    #poll = Poll.create(:questiontext => "A real question?")
+    poll = Poll.create(:questiontext => "A real question?")
 
-    #choice1 = Choice.new(
-    #     :answertext => "NO",
-    #      :is_correct => false
-    # ) 
-    #choice2 = Choice.new(
-    #      :answertext => "YES",
-    #      :is_correct => true
-    # )   
+    choice1 = Choice.new(
+         :answertext => "NO",
+          :is_correct => false
+     ) 
+    choice2 = Choice.new(
+          :answertext => "YES",
+          :is_correct => true
+     )   
 
-     #poll.choices << choice1  
-    # poll.choices << choice2
+    poll.choices << choice1  
+    poll.choices << choice2
  
     respond_to do |format|
       format.html  # index.html.erb
