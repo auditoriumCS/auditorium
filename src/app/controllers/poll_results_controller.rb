b5768a13f4a8cdb53c5614f4199c22ebd9b84b4e
Heinrich Kuhlmann <HeinrichKuhlmann@gmail.com>
2013-05-18 17:50:47 +0200

    add working

--- src/app/controllers/poll_results_controller.rb
+++ src/app/controllers/poll_results_controller.rb
@@ -1,16 +1,47 @@
 class PollResultsController < InheritedResources::Base
 
 	def index
-		poll = Poll.find(params[:id])
+		if params[:id]
+			poll = Poll.find(params[:id])
+		else
+			poll = Poll.all
+		end
 		@poi = poll.choices
 	end		
 	
+	def show
+		if params[:id]
+			poll = Poll.find(params[:id])
+		else
+			poll = Poll.all
+		end
+		@poi = poll.choices
+	end
+	
 	def new
+		@p = Poll.find(params[:questionId])
 		@poll_result = PollResult.new
 
 		respond_to do |format|
 		  format.html  # new.html.erb
-		  format.json  { render :json => @poll }
+		end
+	end
+	
+	def create
+		pr = PollResult.create()
+		pr.userId = params[:poll_result][:userId]
+		pr.questionId = params[:poll_result][:questionId]
+		pr.choiceId = params[:poll_result][:choiceId]
+		pr.save
+		
+		choice = Choice.find(params[:poll_result][:choiceId])
+		choice.poll_result << pr
+		choice.save
+		
+		respond_to do |format|
+		  format.html  # index.html.erb
+		  format.json  { render :json => @pr,
+						:status => :created, :location => @pr }
 		end
 	end
 end		
