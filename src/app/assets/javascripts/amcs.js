0a5e541ea7953d9315b9d103feb1f82a418cc098
Lars Beier <sn3ek@ymail.com>
2013-07-16 12:44:42 +0200

    Revert changes in amcs.js

--- src/app/assets/javascripts/amcs.js
+++ src/app/assets/javascripts/amcs.js
@@ -7,9 +7,9 @@
          */
         //BOSH: "http://localhost/http-bind",
         //BOSH: "/http-bind",
-        BOSH: "http://localhost:7070/http-bind/",
+        BOSH: "http://localhost/http-bind/",
         MUC: "http://jabber.org/protocol/muc",
-        OPENFIREDOMAIN: "127.0.0.1",
+        OPENFIREDOMAIN: "141.76.41.204",
         //ROOM: "test@conference.kp",
         ROOM: null,
 		SLIDE: "acms:ns:slide",
